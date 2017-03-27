f605df1c307a6fdd32feaea9638b9f9f37a9589c
linuxfhy <linuxfhy@163.com>
2017-03-27 18:51:55 +0800

    add rm -rf and mkdir cmd

--- shell/CreateMidplanevpdFile.sh
+++ shell/CreateMidplanevpdFile.sh
@@ -59,6 +59,14 @@ function execcmcipmicmd()
 }
 
 #script start exec from here
+rm -rf /data/midplanevpdcache
+cmd_rc=$?
+[ ${cmd_rc} -eq 0 ] || (echo "command exec fail,cmd:rm -rf /data/midplanevpdcache,cmd_rc:${cmd_rc}"; exit ${cmd_rc})
+
+mkdir /data/midplanevpdcache
+cmd_rc=$?
+[ ${cmd_rc} -eq 0 ] || (echo "command exec fail,cmd:mkdir /data/midplanevpdcache,cmd_rc:${cmd_rc}"; exit ${cmd_rc})
+
 masterCMCip=192.168.200.42
 
 vpdfield=( "0x04 0x2f 0x60 01"  #mtm part 1

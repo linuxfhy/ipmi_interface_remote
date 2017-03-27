694a05093f0eae9fa7adc9b3d12bc1a7d5d6bce6
linuxfhy <linuxfhy@163.com>
2017-03-27 09:37:23 +0800

    change dir structure

---
+++ shell/str_arr_op.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+str=( "Hello"
+	  "my"
+	  "wolrld" )
+for s in ${str[@]}; do
+   echo "$s"
+done
--- tmp.c
+++ tmp.c
@@ -1,39 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct TestStruct
-{
-	int field1;
-	int field2;
-} TestStruct;
-
-TestStruct data []=
-{
-	{0x2+0x3,0x6},
-	{0x8,9},
-};
-
-void main ()
-{
-    /*测试在C语言中执行shell命令*/
-    char cmd0[]="l",cmd1[]="s",cmd[1000];
-	char tmparrary[2][3];
-    int i = 0;
-	char cmdtmp[] = "00000200640105e2";
-	snprintf(cmd,strlen(cmd0)+1,cmd0);
-    snprintf(cmd+strlen(cmd),strlen(cmd1)+1,cmd1);
-    printf("cmd is:\"%s\"\n",cmd);
-    system(cmd);
-
-	printf("data[0].field1=%d\n",data[0].field1);
-	printf("sizeof tmpassary is %d\n",sizeof(tmparrary));
-
-
-	for(i = 0; cmdtmp[i] != '\0'; i++)
-	{
-		printf("0x%02x ",cmdtmp[i]);
-	}
-
-	printf("\n");
+#define DBG_MSG(k,(l)) printf (l)
+void main () {
+    DBG_MSG(1,("Hello World\n"));
 }

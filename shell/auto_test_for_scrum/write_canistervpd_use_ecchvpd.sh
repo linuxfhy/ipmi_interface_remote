c41b95591da39ff0c57720fb0f976756c09d6a5c
linuxfhy <linuxfhy@163.com>
2017-05-03 14:51:53 +0800

    update

--- shell/auto_test_for_scrum/write_canistervpd_use_ecchvpd.sh
+++ shell/auto_test_for_scrum/write_canistervpd_use_ecchvpd.sh
@@ -113,7 +113,7 @@ if [[ ${g_para_1} =~ "w_affec" ]]; then
 
         arr_index_j=0
         while [ $((${arr_index_j})) -lt $((${arr_mem_cnt})) ]; do
-            log "arr_index_j is ${arr_index_j}, arr_index is ${arr_index} before continue"
+            #log "arr_index_j is ${arr_index_j}, arr_index is ${arr_index} before continue"
             if [ "${arr_index_j}" = "${arr_index}" ]; then
                 arr_index_j=$(($arr_index_j+1))
                 continue
@@ -126,7 +126,7 @@ if [[ ${g_para_1} =~ "w_affec" ]]; then
                 log "cmd exec failed,cmd:${readcmd}, cmd_rc:${cmd_rc}"
                 exit ${cmd_rc}
             }
-            log "read_write compare,read:${readresult},write:${tmp_arr[1]}"
+            #log "read_write compare,read:${readresult},write:${tmp_arr[1]}"
             write_data=${tmp_arr[1]}
             if [[ ${tmp_arr[0]} =~ "vpd_can_version_e" ]]
             then
